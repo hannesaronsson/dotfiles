b9298a1f342abf0a0dd593779f6ba3bd6aef61f9
Hannes Aronsson <hannes.aronsson@scania.com>
2025-05-12 09:10:14 +0200

    New nvim config

---
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,24 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = { "html", "cssls", "ruff", "pyright"}
+vim.lsp.enable(servers)
+
+-- read :h vim.lsp.config for changing options of lsp servers 
+-- pyright
+
+local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+local python_path = venv and (venv .. "/bin/python") or "/usr/bin/python3"
+
+vim.lsp.config['pyright'] = {
+  settings = {
+    python = {
+      pythonPath = python_path,
+      venvPath = venv,
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+      -- label = {rainbow = {enabled = true}}
+    },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use default_site
+        keymaps = {
+        insert = "<C-g>z",
+        insert_line = "<C-g>Z",
+        normal = "yz",
+        normal_cur = "yzz",
+        normal_line = "yZ",
+        normal_cur_line = "yZZ",
+        visual = "z",
+        visual_line = "gZ",
+        delete = "dz",
+        change = "cz",
+        change_line = "cZ",
+        }
+        })
+    end
+}
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}

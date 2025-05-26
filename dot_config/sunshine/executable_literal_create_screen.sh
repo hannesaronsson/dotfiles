2f6c0386c090bbbc87bb59764b19d965e7da6a0f
Hannes Aronsson <hannesaronsson@proton.me>
2025-05-26 17:44:33 +0000

    Updated hyprlandconfig

--- dot_config/sunshine/executable_literal_create_screen.sh
+++ dot_config/sunshine/executable_literal_create_screen.sh
@@ -3,11 +3,16 @@
 WIDTH=${1:-1920}
 HEIGHT=${2:-1080}
 REFRESH=${3:-60}
+HDR=${4:-false}
 
 # Create headless and capture its name
 hyprctl output create headless STREAM
 sleep 0.3
 
-hyprctl keyword monitor "STREAM,${WIDTH}x${HEIGHT}@${REFRESH},9999x0,1"
-#hyprctl keyword workspace "10 silent,persistent,monitor:${HEADLESS_NAME}"
-#hyprctl keyword windowrulev2 "workspace 10 silent,class:^(steam)$"
+MONITOR_STRING="STREAM,${WIDTH}x${HEIGHT}@${REFRESH},9999x0,1,vrr,1"
+
+if [[ "$HDR" == "true" ]]; then
+  MONITOR_STRING="${MONITOR_STRING}, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation 1"
+fi
+
+hyprctl keyword monitor "STREAM,${WIDTH}x${HEIGHT}@${REFRESH},9999x0,1, "

fb290bc809222c58d752db7bb7a1d148693cdf2a
Hannes Aronsson <hannes.aronsson@scania.com>
2025-06-25 10:38:48 +0200

    Removed tab completion for nvim cmp (used by copilot)

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,24 +1,33 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {
-      -- label = {rainbow = {enabled = true}}
-    },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			-- wipe the default Tab / S-Tab
+			opts.mapping["<Tab>"] = nil
+			opts.mapping["<S-Tab>"] = nil
+		end,
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- label = {rainbow = {enabled = true}}
+		},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -27,66 +36,65 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use default_site
-        keymaps = {
-        insert = "<C-g>z",
-        insert_line = "<C-g>Z",
-        normal = "yz",
-        normal_cur = "yzz",
-        normal_line = "yZ",
-        normal_cur_line = "yZZ",
-        visual = "z",
-        visual_line = "gZ",
-        delete = "dz",
-        change = "cz",
-        change_line = "cZ",
-        }
-        })
-    end
-},
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-  ft = { "markdown", "norg", "rmd", "org", "codecompanion", "text" },
-  opts = {
-    pipe_table = { style = 'normal' },
-  },
-  config = function(_, opts)
-    require("render-markdown").setup(opts)
-  end,
-},
-{
-  "github/copilot.vim",
-  event = "InsertEnter", -- lazy load on first insert
-}
-  ,
---   {
---    "m4xshen/hardtime.nvim",
---    lazy = false,
---    dependencies = { "MunifTanjim/nui.nvim" },
---    opts = {},
--- },
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use default_site
+				keymaps = {
+					insert = "<C-g>z",
+					insert_line = "<C-g>Z",
+					normal = "yz",
+					normal_cur = "yzz",
+					normal_line = "yZ",
+					normal_cur_line = "yZZ",
+					visual = "z",
+					visual_line = "gZ",
+					delete = "dz",
+					change = "cz",
+					change_line = "cZ",
+				},
+			})
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		ft = { "markdown", "norg", "rmd", "org", "codecompanion", "text" },
+		opts = {
+			pipe_table = { style = "normal" },
+		},
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+		end,
+	},
+	{
+		"github/copilot.vim",
+		event = "InsertEnter", -- lazy load on first insert
+	},
+	--   {
+	--    "m4xshen/hardtime.nvim",
+	--    lazy = false,
+	--    dependencies = { "MunifTanjim/nui.nvim" },
+	--    opts = {},
+	-- },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
